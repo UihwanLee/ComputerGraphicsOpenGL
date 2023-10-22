46f3bde5ab67038dd58bd9785aafbb7c9d14db05
Lee Ui Hwan <luh5063014@naver.com>
2023-10-22 14:19:59 +0900

    [Training20] Training20 기본 뼈대 완성

---
+++ Training20/main.cpp
@@ -0,0 +1,332 @@
+#include <iostream>
+#include "OpenGL.h"
+#include <random>
+#include "InitShader.h"
+#include "ObjectManager.h"
+#include <cmath>
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+// 클래스
+ObjectManager ObjMgr;
+
+GLvoid Init();
+GLvoid Message();
+GLvoid Reset();
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLuint ShaderProgram;
+GLuint VBO[2], EBO;
+bool isDepthTest = false;
+
+GLvoid DrawObjectByArray(int DRAW_TYPE, void* posList, void* colList, int NUM_VETEX, int SIZE_COL);
+GLvoid DrawObjectByIDX(int DRAW_TYPE, void* obj_pos, void* obj_index, void* obj_color, float* pivot, float* rotateInfo, float* scaleInfo,
+	int NUM_VETEX, int SIZE_COL, int SIZE_IDX, glm::mat4& model, float* modelInfo, int idx);
+
+// 변환
+
+// 애니메이션
+GLvoid StopAllAnim();
+
+GLvoid RotatingCamera(int isAnim);
+GLvoid RotatingCamera_Z(int isAnim);
+
+void timer(int value);
+
+bool isRotatingAnim = false;
+
+GLfloat rotateSpeed = 5.0f;
+GLfloat moveSpeed = 0.5f;
+
+bool isFirst = true;
+
+// 투영 변환
+bool projectionMode = true;
+
+// Camera
+GLfloat y_angle = 0.0f, z_angle = 0.0f, camera_y = 0.0f;
+int y_dir = 1, z_dir = 1;
+glm::vec3 CameraPos = glm::vec3(0.5f, 1.0f, 2.0f);
+glm::vec3 AT = glm::vec3(0.0f, 0.0f, 0.0f);
+
+bool rotatingCarmera = false;
+bool rotatingCamera_z = false;
+float rotatingCameraRate = 0.0f;
+float rotatingCameraRate_z = 0.0f;
+
+
+void Keyboard(unsigned char key, int x, int y);
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Training");
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	Init();
+	Message();
+
+	InitProgram(ShaderProgram);
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(Keyboard);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+GLvoid Init()
+{
+	glGenBuffers(2, VBO);
+	glGenBuffers(1, &EBO);
+
+	Reset();
+}
+
+GLvoid Message()
+{
+	cout << "h/H : 은면 제거" << endl;
+	cout << "p/P : 직각 투영/ 원근 투영" << endl;
+	cout << "m/M : 솔리드 모델/와이어 모델" << endl;
+	cout << endl;
+	cout << "w/a/s/d : 좌/우/상/하 이동" << endl;
+	cout << "+/- : 앞/뒤 이동" << endl;
+	cout << "y/Y : 전체 도형 y축으로 양/음 회전" << endl;
+	cout << "z/Z : 행성,달,궤도가 z축에 대하여 양/음 방향으로 회전" << endl;
+	cout << endl;
+}
+
+GLvoid Reset()
+{
+	StopAllAnim();
+	ObjMgr.Reset();
+
+	ObjMgr.CreateCoordinate();
+
+	ObjMgr.CreateCube();
+
+	ObjMgr.SetAllModel();
+}
+
+GLvoid drawScene()
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(ShaderProgram);
+
+	if (isDepthTest) glEnable(GL_DEPTH_TEST);
+	else glDisable(GL_DEPTH_TEST);
+
+	unsigned int projectionLocation = glGetUniformLocation(ShaderProgram, "projectionTransform");
+	unsigned int viewLocation = glGetUniformLocation(ShaderProgram, "viewTransform");
+
+	glm::vec3 cameraPos = glm::vec3(CameraPos.x, CameraPos.y, CameraPos.z); //--- 카메라 위치
+	glm::vec3 cameraDirection = glm::vec3(AT.x, AT.y, AT.z); //--- 카메라 바라보는 방향
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- 카메라 위쪽 방향
+	glm::mat4 view = glm::mat4(1.0f);
+
+	view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);		// 뷰변환
+
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	// 직교투영
+	if (projectionMode)
+	{
+		float left = -1.0f;
+		float right = 1.0f;
+		float bottom = -1.0f;
+		float top = 1.0f;
+		float zNear = -10.0f;
+		float zFar = 10.0f;
+		projection = glm::ortho(left, right, bottom, top, zNear, zFar);
+	}
+	else
+	{
+		// 원근 투영
+		float fov = 45.0f; // 시야각 (Field of View)
+		float aspectRatio = static_cast<float>(WIDTH) / static_cast<float>(HEIGHT); // 화면의 가로 세로 비율
+		float zNear = 0.1f; // 가까운 클리핑 평면
+		float zFar = 50.0f; // 먼 클리핑 평면
+		projection = glm::perspective(glm::radians(fov), aspectRatio, zNear, zFar); //--- 투영 공간 설정: (뷰잉각도, 종횡비, 가까운거리, 먼거리)
+
+	}
+
+	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);		// 투영변환
+
+	for (int i = 0; i < ObjMgr.m_ObjectList.size(); i++)
+	{
+		if (ObjMgr.m_ObjectList[i].m_isActive)
+		{
+			if (ObjMgr.m_ObjectList[i].m_isModeIDX)
+			{
+				DrawObjectByIDX(ObjMgr.m_ObjectList[i].m_DRAW_TYPE, ObjMgr.m_ObjectList[i].m_pos, ObjMgr.m_ObjectList[i].m_inex, ObjMgr.m_ObjectList[i].m_col,
+					ObjMgr.m_ObjectList[i].m_pivot, ObjMgr.m_ObjectList[i].m_rotate, ObjMgr.m_ObjectList[i].m_scale,
+					ObjMgr.m_ObjectList[i].m_num_vertex, ObjMgr.m_ObjectList[i].m_size_pos, ObjMgr.m_ObjectList[i].m_size_idx, ObjMgr.m_ObjectList[i].m_model,
+					ObjMgr.m_ObjectList[i].m_modelInfo, i);
+			}
+			else
+			{
+				DrawObjectByArray(ObjMgr.m_ObjectList[i].m_DRAW_TYPE, ObjMgr.m_ObjectList[i].m_pos, ObjMgr.m_ObjectList[i].m_col,
+					ObjMgr.m_ObjectList[i].m_num_vertex, ObjMgr.m_ObjectList[i].m_size_pos);
+			}
+		}
+	}
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+GLvoid DrawObjectByArray(int DRAW_TYPE, void* posList, void* colList, int NUM_VETEX, int SIZE_COL)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, SIZE_COL, posList, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	unsigned int modelLocation = glGetUniformLocation(ShaderProgram, "modelTransform");
+
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 scale = glm::mat4(1.0f);
+	glm::mat4 rot = glm::mat4(1.0f);
+	glm::mat4 move = glm::mat4(1.0f);
+
+	scale = glm::scale(scale, glm::vec3(1.0f, 1.0f, 1.0f));
+
+	move = glm::translate(move, glm::vec3(0.0f, 0.0f, 0.0f));
+
+	//rot = glm::rotate(rot, glm::radians(-30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	//rot = glm::rotate(rot, glm::radians(-30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	model = model * move * rot * scale;
+
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));		// 모델변환
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, SIZE_COL, colList, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(1);
+
+
+	glDrawArrays(GL_LINES, 0, NUM_VETEX);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+}
+
+GLvoid DrawObjectByIDX(int DRAW_TYPE, void* obj_pos, void* obj_index, void* obj_color, float* pivot, float* rotateInfo, float* scaleInfo,
+	int NUM_VETEX, int SIZE_COL, int SIZE_IDX, glm::mat4& model, float* modelInfo, int idx)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, SIZE_COL, obj_pos, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, SIZE_IDX, obj_index, GL_STATIC_DRAW);
+
+	// 모델변환
+	unsigned int modelLocation = glGetUniformLocation(ShaderProgram, "modelTransform");
+
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, SIZE_COL, obj_color, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+
+	glDrawElements(DRAW_TYPE, NUM_VETEX, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+}
+
+GLvoid StopAllAnim()
+{
+
+}
+
+float angle_camera = 0;
+GLvoid RotatingCamera(int isAnim)
+{
+	float radius = 5.0f;
+	CameraPos.x = sin(angle_camera) * radius;
+	CameraPos.z = cos(angle_camera) * radius;
+
+	angle_camera += 0.03f;
+
+	glutPostRedisplay();
+
+	if (rotatingCarmera) glutTimerFunc(30, RotatingCamera, rotatingCarmera);
+}
+
+float angle_camera_z = 0;
+GLvoid RotatingCamera_Z(int isAnim)
+{
+	float radius = 5.0f;
+	CameraPos.x = sin(angle_camera_z) * radius;
+	CameraPos.y = cos(angle_camera_z) * radius;
+
+	angle_camera_z += 0.03f;
+
+	glutPostRedisplay();
+
+	if (rotatingCamera_z) glutTimerFunc(30, RotatingCamera_Z, rotatingCamera_z);
+}
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'H':
+	case 'h':
+		// 은면 제거
+		isDepthTest = !isDepthTest;
+		break;
+	case 'Y':
+	case 'y':
+		rotatingCamera_z = false;
+		if (rotatingCarmera) rotatingCarmera = false;
+		else if (rotatingCarmera == false) rotatingCarmera = true;
+		if (rotatingCarmera) glutTimerFunc(30, RotatingCamera, rotatingCarmera);
+		break;
+	case 'P':
+	case 'p':
+		projectionMode = !projectionMode;
+		break;
+	case 'C':
+	case 'c':
+		Reset();
+		break;
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
