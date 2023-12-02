cda96ed26c64d3b38391443b3c4e4dd58c3032b7
Lee Ui Hwan <luh5063014@naver.com>
2023-12-02 23:21:37 +0900

    [Training32] 뒷 배경으로 SKY 텍스처 적용 완료.

---
+++ Training32/Object.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "OpenGL.h"
+#include <iostream>
+
+#define SIZE_CONE_VERTEX 66
+#define SIZE_CONE_INDEX 60
+
+#define SIZE_SQHERE_VERTEX 1323
+#define SIZE_SQHERE_INDEX 2400
+
+#define SIZE_CYLINDER_VERTEX 2166
+#define SIZE_CYLINDER_INDEX 2160
+
+#define CONE_HEIGHT 1.0
+#define RADIUS 0.7
+#define STACKS 20
+#define SLICES 20
+#define PI 3.1415926535
+
+class Object
+{
+public:
+	// ÁÂÇ¥Æò¸é
+	static GLfloat	CoordinateVertexs[18];
+	static GLfloat	CoordinateColors[18];
+
+	// Á¤À°¸éÃ¼
+	static GLfloat	CubeVertexs[108];
+	static GLfloat	CubeNormal[108];
+	static GLint	CubeIndexs[36];
+
+	// Á¤»ç¸éÃ¼
+	static GLfloat	TetrahedronVertexs[12];
+	static GLfloat	TetrahedronColors[12];
+	static GLint	TetrahedronIndexs[12];
+
+	// Á¤»ç°¢»Ô
+	static GLfloat	SquarePyramidVertexs[15];
+	static GLfloat	SquarePyramidNormal[15];
+	static GLint	SquarePyramidIndexs[18];
+
+	// ¿ø»Ô
+	static GLfloat	ConeVertexs[SIZE_CONE_VERTEX];
+	static GLfloat	ConeColors[SIZE_CONE_VERTEX];
+	static GLint	ConeIndexs[SIZE_CONE_INDEX];
+
+	// ¿ø
+	static GLfloat	SqhereVertexs[SIZE_SQHERE_VERTEX];
+	static GLfloat	SqhereColors[SIZE_SQHERE_VERTEX];
+	static GLint	SqhereIndexs[SIZE_SQHERE_INDEX];
+};
+
+
+
+
---
+++ Training32/main.cpp
@@ -0,0 +1,508 @@
+#include <iostream>
+#include "OpenGL.h"
+#include <random>
+#include "InitShader.h"
+#include "ObjectManager.h"
+#include "Timer.h"
+#include <cmath>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+// 클래스
+ObjectManager ObjMgr;
+Timer timer;
+
+// 타이머
+float startTime = 0.0f;
+float prevTime = 0.0f;
+GLvoid UpdateRender();
+
+GLvoid Init();
+GLvoid Message();
+GLvoid Reset();
+GLvoid drawScene(GLvoid);
+GLvoid drawModel();
+GLvoid drawProjection();
+GLvoid drawView();
+GLvoid Reshape(int w, int h);
+GLuint ShaderProgram;
+GLuint VBO[3], EBO;
+GLuint texture;
+bool isDepthTest = true;
+
+GLvoid drawLight();
+GLvoid DrawObjectByIDX(int DRAW_TYPE, glm::mat4& model, int idx);
+
+// 애니메이션 :: 초기화
+GLvoid StopAllAnim();
+
+bool isRotatingFigure = false;
+GLvoid RotatingFigure(int idx);
+int figure_idx = 0;
+
+// 애니메이션 :: 카메라
+GLvoid RotatingCamera(int isAnim);
+GLvoid RotatingCamera_X(int isAnim);
+
+float radius = 0.5f;
+
+// 애니메이션 :: 변수
+GLvoid CheckCollision();
+bool CheckCollisionByBox(float x, float y, float z);
+
+bool isLight = true;
+
+// pre 위치
+GLfloat prevPivot[3];
+bool isCollisionUp = false;
+
+// 투영 변환
+bool projectionMode = true;
+
+// Camera
+GLfloat y_angle = 0.0f, z_angle = 0.0f, camera_y = 0.0f;
+int y_dir = 1, z_dir = 1;
+glm::vec3 CameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 AT = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 LightPos = glm::vec3(0.5f, 0.0f, 0.0f);
+glm::vec3 LightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+glm::vec3 LightVolume = glm::vec3(0.5f, 0.5f, 0.5f);
+
+float roatingLightDir = 1.0f;
+bool isShowCube = true;
+bool rotatingLight = false;
+bool rotatingCarmera = false;
+bool rotatingCarmera_X = false;
+bool rotatingCamera_z = false;
+float rotatingCameraRate = 0.0f;
+float rotatingCameraRate_x = 0.0f;
+float rotatingCameraRate_y = 0.0f;
+
+void Keyboard(unsigned char key, int x, int y);
+void keyboardUp(unsigned char key, int x, int y);
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Training");
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	Init();
+	Message();
+
+	InitProgram(ShaderProgram);
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(Keyboard);
+	glutKeyboardUpFunc(keyboardUp);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+GLvoid Init()
+{
+	glGenBuffers(3, VBO);
+	glGenBuffers(1, &EBO);
+	glGenTextures(2, &texture);
+
+	Reset();
+
+	startTime = glutGet(GLUT_ELAPSED_TIME); // 시작 시간 설정
+}
+
+// 시간을 계산하여 elapsedTime 계산
+GLvoid UpdateRender()
+{
+	int currentTime = glutGet(GLUT_ELAPSED_TIME);
+	float elapsedTime = 0.0f;
+
+	if (prevTime == 0.0f)
+	{
+		elapsedTime = currentTime - currentTime;
+	}
+	else
+	{
+		elapsedTime = currentTime - prevTime;
+	}
+	prevTime = currentTime;
+
+	timer.SetTimer(elapsedTime);
+
+	if (ObjMgr.m_ObjectList.size() >= 14)
+	{
+		//ObjMgr.UpdatePos(7, elapsedTime);
+		//ObjMgr.Move(7, 0.0f, -0.1f, 0.0f);
+
+		//CheckCollision();
+	}
+}
+
+GLvoid Message()
+{
+	cout << endl;
+	cout << "c: 육면체" << endl;
+	cout << "p: 사각뿔" << endl;
+	cout << "x/X: x축을 중심으로 회전" << endl;
+	cout << "y/Y: y축을 중심으로 회전" << endl;
+	cout << endl;
+	cout << "q: 프로그램 종료" << endl;
+	cout << endl;
+}
+
+GLvoid Reset()
+{
+	StopAllAnim();
+	ObjMgr.Reset();
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_01);
+	ObjMgr.SetScale(0, 0.01f, 0.1f, 0.1f);
+	ObjMgr.SetPosition(0, 5.0f, 0.0f, 0.0f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_02);
+	ObjMgr.SetScale(1, 0.1f, 0.1f, 0.01f);
+	ObjMgr.SetPosition(1, 0.0f, 0.0f, 4.5f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_03);
+	ObjMgr.SetScale(2, 0.1f, 0.1f, 0.01f);
+	ObjMgr.SetPosition(2, 0.0f, 0.0f, -4.5f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_04);
+	ObjMgr.SetScale(3, 0.01f, 0.1f, 0.095f);
+	ObjMgr.SetPosition(3, -4.6f, 0.0f, 0.0f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_04);
+	ObjMgr.SetScale(4, 0.1f, 0.01f, 0.09f);
+	ObjMgr.SetPosition(4, 0.0f, 4.53f, 0.0f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::CUBE_04);
+	ObjMgr.SetScale(5, 0.1f, 0.01f, 0.09f);
+	ObjMgr.SetPosition(5, 0.0f, -4.53f, 0.0f);
+
+	ObjMgr.CreateCube(1.0f, 1.0f, 1.0f, ObjectType::SKY);
+	ObjMgr.SetScale(6, 0.01f, 5.0f, 5.0f);
+	ObjMgr.SetPosition(6, -10.0f, 0.0f, 0.0f);
+
+
+	CameraPos = glm::vec3(0.5f, 0.2f, 0.2f);
+}
+
+GLvoid drawScene()
+{
+	UpdateRender();
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(ShaderProgram);
+
+	if (isDepthTest) glEnable(GL_DEPTH_TEST);
+	else glDisable(GL_DEPTH_TEST);
+
+	drawView();
+	drawProjection();
+	drawLight();
+	drawModel();
+
+	glutSwapBuffers();
+}
+
+GLvoid drawView()
+{
+	unsigned int viewLocation = glGetUniformLocation(ShaderProgram, "viewTransform");
+
+	glm::vec3 cameraPos = glm::vec3(CameraPos.x, CameraPos.y, CameraPos.z); //--- 카메라 위치
+	glm::vec3 cameraDirection = glm::vec3(AT.x, AT.y, AT.z); //--- 카메라 바라보는 방향
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //--- 카메라 위쪽 방향
+	glm::mat4 view = glm::mat4(1.0f);
+
+	view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);		// 뷰변환
+}
+
+GLvoid drawProjection()
+{
+	unsigned int projectionLocation = glGetUniformLocation(ShaderProgram, "projectionTransform");
+
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	// 직교투영
+	if (projectionMode == false)
+	{
+		float left = -1.0f;
+		float right = 1.0f;
+		float bottom = -1.0f;
+		float top = 1.0f;
+		float zNear = -10.0f;
+		float zFar = 10.0f;
+		projection = glm::ortho(left, right, bottom, top, zNear, zFar);
+	}
+	else
+	{
+		// 원근 투영
+		float fov = 45.0f; // 시야각 (Field of View)
+		float aspectRatio = static_cast<float>(WIDTH) / static_cast<float>(HEIGHT); // 화면의 가로 세로 비율
+		float zNear = 0.1f; // 가까운 클리핑 평면
+		float zFar = 50.0f; // 먼 클리핑 평면
+		projection = glm::perspective(glm::radians(fov), aspectRatio, zNear, zFar); //--- 투영 공간 설정: (뷰잉각도, 종횡비, 가까운거리, 먼거리)
+
+	}
+
+	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);		// 투영변환
+}
+
+GLvoid drawLight()
+{
+	unsigned int lightPosLocation = glGetUniformLocation(ShaderProgram, "lightPos"); //--- lightPos 값 전달: (0.0, 0.0, 5.0);
+	glUniform3f(lightPosLocation, LightPos.x, LightPos.y, LightPos.z);
+	unsigned int lightColorLocation = glGetUniformLocation(ShaderProgram, "lightColor"); //--- lightColor 값 전달: (1.0, 1.0, 1.0) 백색
+	glUniform3f(lightColorLocation, LightColor.r, LightColor.g, LightColor.b);
+	unsigned int viewPosLocation = glGetUniformLocation(ShaderProgram, "viewPos"); //--- viewPos 값 전달: 카메라 위치
+	glUniform3f(viewPosLocation, 0.0f, 0.0f, 0.0f);
+}
+
+GLvoid drawModel()
+{
+	for (int i = 0; i < ObjMgr.m_ObjectList.size(); i++)
+	{
+		if (ObjMgr.m_ObjectList[i].m_isActive)
+		{
+			DrawObjectByIDX(ObjMgr.m_ObjectList[i].m_DRAW_TYPE, ObjMgr.m_ObjectList[i].m_model, i);
+		}
+	}
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+GLvoid DrawObjectByIDX(int DRAW_TYPE, glm::mat4& model, int idx)
+{
+	// vPos
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, ObjMgr.m_ObjectList[idx].vertices.size() * sizeof(glm::vec3), &ObjMgr.m_ObjectList[idx].vertices[0], GL_STATIC_DRAW);
+
+	// 모델변환
+	unsigned int modelLocation = glGetUniformLocation(ShaderProgram, "modelTransform");
+	ObjMgr.m_ObjectList[idx].m_model = ObjMgr.TransformModel(idx);
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(ObjMgr.m_ObjectList[idx].m_model));
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+
+	// vNormal
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, ObjMgr.m_ObjectList[idx].normals.size() * sizeof(glm::vec3), &ObjMgr.m_ObjectList[idx].normals[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+
+	// color
+	unsigned int objColorLocation = glGetUniformLocation(ShaderProgram, "objectColor"); //--- object Color값 전달: (1.0, 0.5, 0.3)의 색
+	glUniform3f(objColorLocation, ObjMgr.m_ObjectList[idx].m_color[0], ObjMgr.m_ObjectList[idx].m_color[1], ObjMgr.m_ObjectList[idx].m_color[2]);
+
+	// vTexture
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, ObjMgr.m_ObjectList[idx].uvs.size() * sizeof(glm::vec2), &ObjMgr.m_ObjectList[idx].uvs[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), 0);
+
+	// Texture
+	int widthImage, heightImage, numberOfChannel;
+	// stbi_set_flip_vertically_on_load(true); //--- 이미지가 거꾸로 읽힌다면 추가
+	unsigned char* data = stbi_load("white.png", &widthImage, &heightImage, &numberOfChannel, 0);
+
+	if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::DEFAULT)
+	{
+		data = stbi_load("white.png", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_01 || ObjMgr.m_ObjectList[idx].m_type == ObjectType::PYRAMID_01)
+	{
+		data = stbi_load("Texture01.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_02 || ObjMgr.m_ObjectList[idx].m_type == ObjectType::PYRAMID_02)
+	{
+		data = stbi_load("Texture02.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_03 || ObjMgr.m_ObjectList[idx].m_type == ObjectType::PYRAMID_03)
+	{
+		data = stbi_load("Texture03.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_04 || ObjMgr.m_ObjectList[idx].m_type == ObjectType::PYRAMID_04)
+	{
+		data = stbi_load("Texture04.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_05 || ObjMgr.m_ObjectList[idx].m_type == ObjectType::PYRAMID_05)
+	{
+		data = stbi_load("Texture05.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::CUBE_06)
+	{
+		data = stbi_load("Texture06.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+	else if (ObjMgr.m_ObjectList[idx].m_type == ObjectType::SKY)
+	{
+		data = stbi_load("sky.jpg", &widthImage, &heightImage, &numberOfChannel, 0);
+	}
+
+	unsigned int objTextureLocation = glGetUniformLocation(ShaderProgram, "outTexture");
+	glUniform1i(objTextureLocation, 0);
+
+	glActiveTexture(GL_TEXTURE0); //--- 유닛 0을 활성화
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, widthImage, heightImage, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+	glDrawArrays(DRAW_TYPE, 0, ObjMgr.m_ObjectList[idx].vertices.size());
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	stbi_image_free(data);
+}
+
+float angle_camera_x = 0;
+float angle_camera = 0;
+GLvoid StopAllAnim()
+{
+	angle_camera_x = 0;
+	angle_camera = 0;
+	rotatingCarmera = false;
+	rotatingCarmera_X = false;
+}
+
+GLvoid RotatingFigure(int idx)
+{
+	ObjMgr.Rotate(idx, 0.0f, 0.5f, 0.0f);
+
+	glutPostRedisplay();
+
+	if (isRotatingFigure) glutTimerFunc(30, RotatingFigure, idx);
+}
+
+GLvoid RotatingCamera(int isAnim)
+{
+	float radius = 1.0f;
+	CameraPos.x = sin(angle_camera) * radius;
+	CameraPos.z = cos(angle_camera) * radius;
+
+	angle_camera += 0.03f;
+
+	glutPostRedisplay();
+
+	if (rotatingCarmera) glutTimerFunc(30, RotatingCamera, rotatingCarmera);
+}
+
+GLvoid RotatingCamera_X(int isAnim)
+{
+	float radius = 1.0f;
+	CameraPos.y = sin(angle_camera_x) * radius;
+	CameraPos.z = cos(angle_camera_x) * radius;
+
+	angle_camera_x += 0.03f;
+
+	glutPostRedisplay();
+
+	if (rotatingCarmera_X) glutTimerFunc(30, RotatingCamera_X, rotatingCarmera_X);
+}
+
+float angle_light = 0;
+GLvoid RotatingLight(int isAnim)
+{
+	LightPos.x = sin(angle_light) * radius * roatingLightDir;
+	LightPos.z = cos(angle_light) * radius;
+
+	angle_light += 0.03f;
+
+	glutPostRedisplay();
+
+
+	if (rotatingLight) glutTimerFunc(30, RotatingLight, rotatingLight);
+}
+
+GLvoid ChangeLightRandomColor()
+{
+	float r = ObjMgr.GetRandomFloatValue(0.0f, 1.0f);
+	float g = ObjMgr.GetRandomFloatValue(0.0f, 1.0f);
+	float b = ObjMgr.GetRandomFloatValue(0.0f, 1.0f);
+	LightColor.r = r;
+	LightColor.g = g;
+	LightColor.b = b;
+}
+
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	float force_x, force_y, force_z;
+	force_x = force_y = force_z = 0.f;
+	bool input_w = false;
+	switch (key)
+	{
+	case 'H':
+	case 'h':
+		// 은면 제거
+		if (isDepthTest) isDepthTest = false;
+		else isDepthTest = true;
+		break;
+	case 'X':
+	case 'x':
+		StopAllAnim();
+		if (rotatingCarmera_X) rotatingCarmera_X = false;
+		else if (rotatingCarmera_X == false) { rotatingCarmera_X = true; }
+		if (rotatingCarmera_X) glutTimerFunc(30, RotatingCamera_X, rotatingCarmera_X);
+		break;
+	case 'Y':
+	case 'y':
+		StopAllAnim();
+		if (rotatingCarmera) rotatingCarmera = false;
+		else if (rotatingCarmera == false) { rotatingCarmera = true; }
+		if (rotatingCarmera) glutTimerFunc(30, RotatingCamera, rotatingCarmera);
+		break;
+	case 'S':
+	case 's':
+		StopAllAnim();
+		CameraPos = glm::vec3(0.5f, 0.2f, 0.2f);
+		break;
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void keyboardUp(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
